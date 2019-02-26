c908c8529d175f277255f236c381bef02d7cccab
Hector Aguilar <ah91086@berkeley.edu>
2019-02-25 18:04:57 -0800

    basically finished?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,31 +11,111 @@ class MoviesController < ApplicationController
   end
 
   def index
+    # session.clear
+
+    @test  = true
     @all_ratings = Movie.get_ratings
     shouldInclude = []
     retrievedHash = params["ratings"]
-    if retrievedHash != nil
+    if !session.key?(:first_iteration_passed)
+      session[:first_iteration_passed] = true
+      session[:filterSettings] =  @all_ratings
+      session[:orderByHeader] = false
+      session[:orderByDate] = false
+    end
+
+    
+    shouldInclude = []
+    retrievedHash = params["ratings"]
+
+
+    # if @first_iteration
+    #   @first_iteration = false
+    #   session[:filterSettings] = 
+
+    # session[:filterSettings]
+
+
+
+
+
+
+
+    #we set the default value of check boxes to checked but idk if thats reflected in the retrievedHash
+
+    retrievedHash = params["ratings"]
+    if retrievedHash == nil ||  retrievedHash.length == 0
+      shouldInclude = session[:filterSettings]
+    else
+      # if retrievedHash != nil
+
       for rating in @all_ratings do 
         if retrievedHash.key?(rating)
           shouldInclude.push(rating)
         end
       end
+
+
+      # end
+
+      session[:filterSettings] = shouldInclude
+
     end
+
+
+
+
+
+
+
+
+
 
     @movie_title_yellow = false
     @release_date_yellow = false
 
+    # if @first_iteration
+    #   @first_iteration = false
+    #   shouldInclude = Movie.get_ratings
+
+    #   @movie_title_yellow = true
+    #   @release_date_yellow = true
+    # end
+
+    # @movie_title_yellow = true
+    # @release_date_yellow = true
+
+
+
     if params[:orderByHeader]
+      session[:orderByHeader] = true
+      session[:orderByDate] = false
+
       # @movies = Movie.order(:title)
       @movies = Movie.with_ratings(shouldInclude, :title)
       @movie_title_yellow = true
     elsif params[:orderByDate] 
       # @movies = Movie.order(:release_date)
+      session[:orderByDate] = true
+      session[:orderByHeader] = false
+
       @movies = Movie.with_ratings(shouldInclude, :release_date)
       @release_date_yellow = true
     else
       @movies = Movie.with_ratings(shouldInclude, nil)
     end
+
+
+    if session[:orderByHeader] 
+      @movie_title_yellow = true
+      @movies = Movie.with_ratings(shouldInclude, :title)
+    end
+
+    if session[:orderByDate]
+      @release_date_yellow = true
+      @movies = Movie.with_ratings(shouldInclude, :release_date)
+    end
+
   end
 
 
